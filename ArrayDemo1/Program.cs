18421df3778f181fc89869dc84173d49053ea06b
Jeff Cogswell <jeffcogs@gmail.com>
2021-11-22 14:51:34 -0500

    Added ArrayDemo2

--- ArrayDemo1/Program.cs
+++ ArrayDemo1/Program.cs
@@ -6,7 +6,42 @@ namespace ArrayDemo1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] numbers = new int[] { 5, 10, 15, 20, 25 };
+			for (int i=0; i<5; i++)
+			{
+				Console.WriteLine(numbers[i]);
+			}
+
+			Console.WriteLine("Now let's make a string array");
+			string[] names = new string[] { "Fred", "Sally", "Julia", "Sam" };
+			Console.WriteLine(names[0]); // Print out the first one
+			Console.WriteLine(names[2]); // Print out the third one
+
+			Console.WriteLine("Change one and print them all out");
+			// Let's suppose we need to change one
+			// Let's change Sam to Samantha
+			names[3] = "Samantha";
+			for (int i=0; i < names.Length; i++)
+			{
+				Console.WriteLine(names[i]);
+			}
+
+			// Let's create an array but not initialize it. We do, however,
+			// have to state the size when we create it.
+			// We'll create an array with four slots, but intially they
+			// are empty
+			Console.WriteLine("Create an array without initializing it.");
+			string[] words = new string[4];
+			words[0] = "Apple";
+			words[1] = "Banana";
+			words[2] = "Orange";
+			words[3] = "Watermelon";
+
+			for (int i = 0; i < words.Length; i++)
+			{
+				Console.WriteLine(words[i]);
+			}
+
 		}
 	}
 }
---
+++ ArrayDemo2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ArrayDemo2
+{
+	class Program
+	{
+		static void PrintMenu(string[] theItems, decimal[] thePrices)
+		{
+			Console.WriteLine("Here is our menu:");
+			for (int i = 0; i < theItems.Length; i++)
+			{
+				Console.WriteLine($"{theItems[i],-12}{thePrices[i],-6}");
+			}
+		}
+
+		static int FindItem(string[] theItems, string search)
+		{
+			for (int i = 0; i<theItems.Length; i++)
+			{
+				if (theItems[i].ToLower() == search.ToLower())
+				{
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		static void Main(string[] args)
+		{
+			/*
+			 * 
+			 *   Create a menu used at a coffee house
+			 *   
+			 *         Item              Price
+			 *         ----              -----
+			 *         Coffee            2.00
+			 *         Espresso          2.50
+			 *         Muffin            3.50
+			 *         Croissant         4.00
+			 * 
+			 */
+
+			// For now we're creating two separate arrays
+			string[] items = new string[] { "Coffee", "Espresso", "Muffin", "Croissant" };
+			decimal[] prices = new decimal[] { 2.00m, 2.50m, 3.50m, 4.00m };
+
+			PrintMenu(items, prices);
+
+			Console.WriteLine("Please make a selection:");
+			string entry = Console.ReadLine();
+			int index = FindItem(items, entry);
+
+			//Console.WriteLine(index);
+
+			if (index > -1)
+			{
+				Console.WriteLine($"The price of {items[index]} is {prices[index]}");
+			}
+			else
+			{
+				Console.WriteLine($"Sorry, we do not have {entry}.");
+			}
+
+			//for (int i = 0; i < items.Length; i++)
+			//{
+			//	if (items[i].ToLower() == entry.ToLower())
+			//	{
+			//		Console.WriteLine($"The price of {items[i]} is {prices[i]}");
+			//		break;
+			//	}
+			//}
+		}
+	}
+}
